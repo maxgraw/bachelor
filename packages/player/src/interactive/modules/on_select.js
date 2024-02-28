6507810d2c2104d8ac625d8703df98d636404c15
Max Graw <max@graw-online.de>
2024-02-28 12:43:42 +0100

    remove logs

--- packages/player/src/interactive/modules/on_select.js
+++ packages/player/src/interactive/modules/on_select.js
@@ -3,12 +3,7 @@ import * as THREE from "three";
 export function on_select() {
   const data = this.raycast(this.raycaster.controller, [this.cursor]);
 
-  console.log(data)
-
-  console.log(this.cursor.children[1])
-
   if (data) {
-    console.log("select")
     const position = new THREE.Vector3();
     position.setFromMatrixPosition(this.cursor.matrix);
     this.menu.position.copy(position);
